cb5dc5347a8f514b9d35468faa2892859a545d25
Khanh <knh.nguyen2002@gmail.com>
2023-03-05 08:57:26 -0600

    Coded JS display temperature of searched city

--- src/script.js
+++ src/script.js
@@ -15,6 +15,7 @@ function display(event) {
 let search = document.querySelector("#search");
 search.addEventListener("submit", display);
 
+
 // include current time on app (day and time)
 function formatDate(now) {
   let hours = now.getHours();
@@ -44,3 +45,32 @@ function formatDate(now) {
 let dayMain = document.querySelector("#day-main");
 let currentTime = new Date();
 dayMain.innerHTML = formatDate(currentTime);
+
+
+ // display current temperature of searched city
+ function displayWeather(response) {
+    let tempFahrenheit = document.querySelector("#temp-main");
+    tempFahrenheit.innerHTML = Math.round(response.data.main.temp);
+  
+    let weatherStatus = document.querySelector("#weather-status");
+    weatherStatus.innerHTML = response.data.weather[0].main;
+  
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  
+    let windSpeed = document.querySelector("#wind-speed");
+    windSpeed.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} mph`
+  
+  }
+  function getWeatherInfo() {
+    let cityInput = document.querySelector("#city-input");
+    let city = cityInput.value;
+    let apiKey = "f5e814a04eddfab1740f07bf0328eee2";
+    let units = "imperial";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(displayWeather);
+  };
+  let weather = document.querySelector("#search");
+  weather.addEventListener("submit", getWeatherInfo)
+  
+  
