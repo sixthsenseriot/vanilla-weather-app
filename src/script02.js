d66bbce7a6a1fbaa3eb0d706795aea584e957835
Khanh <knh.nguyen2002@gmail.com>
2023-06-19 04:02:43 -0500

    Added notes to functions in JS code

--- src/script02.js
+++ src/script02.js
@@ -1,3 +1,4 @@
+// include current day and time
 function formatDate(now) {
     let date = new Date(now);
     let hours = date.getHours();
@@ -26,23 +27,25 @@ let currentTime = new Date();
 dayMain.innerHTML = formatDate(currentTime);
 
 
-
+// display weather of city
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temp-main");
     let cityElement = document.querySelector("#display-city");
     let statusElement = document.querySelector("#weather-status");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind-speed");
-
-    let fahrenheitTemperature = response.data.main.temp;
-
+  
+    fahrenheitTemperature = response.data.main.temp;
+  
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
     cityElement.innerHTML = response.data.name;
     statusElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = `Humidity ${response.data.main.humidity}%`;
     windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed * 3.6)} mph`;
-}
+  }
 
+
+// api key and url to search
 function search(city) {
     let apiKey = "bc2cd97eaa209e7d22d8f3c84081655f";
     let units = "imperial";
@@ -50,6 +53,8 @@ function search(city) {
     axios.get(apiUrl).then(displayTemperature);
 }
 
+
+// handle user submission
 function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
@@ -64,6 +69,8 @@ function handleSubmit(event) {
     }
 }
 
+
+// display celsius temperature
 function displayCelsiusTemperature(event) {
     event.preventDefault();
     let temperatureElement = document.querySelector("#temp-main");
@@ -71,6 +78,8 @@ function displayCelsiusTemperature(event) {
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+
+// display fahrenheit temperature
 function displayFahrenheitTemperature(event) {
     event.preventDefault();
     let temperatureElement = document.querySelector("#temp-main");
